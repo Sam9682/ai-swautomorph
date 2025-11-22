c788a8b2bc3fb8550ca092bcdd8f938afef5014e
Sam9682 <lepetre@yahoo.fr>
2025-11-22 15:14:57 +0000

    Porxy nginx /gitea

--- deploy.sh
+++ deploy.sh
@@ -376,7 +376,7 @@ ROOT = /home/ubuntu/admin/data/gitea-repositories
 DOMAIN = www.swautomorph.com
 HTTP_ADDR = 0.0.0.0
 HTTP_PORT = 3000
-ROOT_URL = http://www.swautomorph.com:3000/
+ROOT_URL = https://www.swautomorph.com/gitea/
 
 [mailer]
 ENABLED = false
@@ -510,6 +510,14 @@ server {
     
     location / {
         proxy_pass http://127.0.0.1:5001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+    
+    location /gitea/ {
+        proxy_pass http://127.0.0.1:3000/;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
--- src/gitea_config.py
+++ src/gitea_config.py
@@ -4,7 +4,7 @@
 GITEA_SERVER = "http://localhost:3000"  # Your Gitea server URL
 ADMIN_TOKEN = "your_admin_token_here"   # Admin user token for creating users
 USER_TOKEN = "your_user_token_here"     # User token for creating repos
-USERNAME = "admin"
-EMAIL = "admin@example.com"
-PASSWORD = "secure_password_here"       # Password for the user
+USERNAME = "gitadmin"
+EMAIL = "admin@swautomorph.com"
+PASSWORD = "password"       # Password for the user
 REPO_NAME = "ai-checkinatwork"
