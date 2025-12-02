72ea8b34f5dc12884fbdd011ed48d81a8c072620
Sam9682 <lepetre@yahoo.fr>
2025-12-02 23:59:03 +0000

    add newgrp docker

--- init_pltf.sh
+++ init_pltf.sh
@@ -17,6 +17,7 @@ echo 'export PATH="/home/ubuntu/.local/bin:$PATH"' >> ~/.bashrc
 
 # Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh
+newgrp docker
 sudo usermod -aG docker $USER
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
 
