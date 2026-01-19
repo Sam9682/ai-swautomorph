f112d0625b60747ee7d1cdd4bf7f2265d8d41bff
Sam9682 <lepetre@yahoo.fr>
2026-01-19 15:07:49 +0000

    bugfix init_pltf

--- init_pltf.sh
+++ init_pltf.sh
@@ -14,7 +14,6 @@ curl -fsSL https://cli.kiro.dev/install | bash
 curl -fsSL https://raw.githubusercontent.com/ovh/shai/main/install.sh | sh
 echo 'export PATH="/home/ubuntu/.local/bin:$PATH"' >> ~/.bashrc
 
-
 # Install AWS shai
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
@@ -25,9 +24,6 @@ curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh
 newgrp docker
 sudo usermod -aG docker $USER
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
-
-mkdir logs
-chmod +x setup_modsecurity_config.sh
 
 # Create a config file in ~/.aws/config
 mkdir -p ~/.aws
@@ -61,5 +57,8 @@ source .venv/bin/activate
 
 # Install Python requirements
 pip install -r requirements.txt
+
+mkdir logs
+chmod +x setup_modsecurity_config.sh
 
 echo "Modify the name of the PLTF in ./conf/deploy.ini !"
